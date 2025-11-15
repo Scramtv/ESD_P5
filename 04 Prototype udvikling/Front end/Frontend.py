a0636f8614734d8ff1f4462b37217aebeb06b56f
LilKoch <nhdn23@student.aau.dk>
2025-11-15 16:08:20 +0100

    Slet af fejl kode og opdatering af ny kode
    -Jeppe på pi

--- 04 Prototype udvikling/Front end/Frontend.py
+++ 04 Prototype udvikling/Front end/Frontend.py
@@ -9,6 +9,8 @@
 # Author: lille-koch
 # GNU Radio version: 3.10.12.0
 
+from gnuradio import analog
+from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
@@ -17,8 +19,8 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio import network
 from gnuradio import soapy
-from gnuradio import zeromq
 import threading
 
 
@@ -35,22 +37,20 @@ class Frontend(gr.top_block):
         ##################################################
         self.samp_rate = samp_rate = 61.44e6
         self.cent_freq = cent_freq = 2441.75e6
-        self.band_pass = band_pass = 56e6
-        self.RF_gain = RF_gain = 10
+        self.band_pass = band_pass = 10e6
+        self.RF_gain = RF_gain = 1
 
         ##################################################
         # Blocks
         ##################################################
 
-        self.zeromq_pub_sink_0_0 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, 'tcp://*:5556', 100, False, (-1), '', True, True)
-        self.zeromq_pub_sink_0 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, 'tcp://*:5555', 100, False, (-1), '', True, True)
         self.soapy_bladerf_source_0_0 = None
         dev = 'driver=bladerf'
         stream_args = ''
         tune_args = ['']
         settings = ['']
 
-        self.soapy_bladerf_source_0_0 = soapy.source(dev, "fc32", 1, 'bladrf=0,channel=1',
+        self.soapy_bladerf_source_0_0 = soapy.source(dev, "fc32", 1, 'bladrf=0,channel=0',
                                   stream_args, tune_args, settings)
         self.soapy_bladerf_source_0_0.set_sample_rate(0, samp_rate)
         self.soapy_bladerf_source_0_0.set_bandwidth(0, band_pass)
@@ -63,20 +63,46 @@ class Frontend(gr.top_block):
         tune_args = ['']
         settings = ['']
 
-        self.soapy_bladerf_source_0 = soapy.source(dev, "fc32", 1, 'bladrf=0,channel=0',
+        self.soapy_bladerf_source_0 = soapy.source(dev, "fc32", 1, 'bladrf=0,channel=1',
                                   stream_args, tune_args, settings)
         self.soapy_bladerf_source_0.set_sample_rate(0, samp_rate)
         self.soapy_bladerf_source_0.set_bandwidth(0, band_pass)
         self.soapy_bladerf_source_0.set_frequency(0, cent_freq)
         self.soapy_bladerf_source_0.set_frequency_correction(0, 0)
         self.soapy_bladerf_source_0.set_gain(0, min(max(RF_gain, -1.0), 60.0))
+        self.soapy_bladerf_sink_0 = None
+        dev = 'driver=bladerf'
+        stream_args = ''
+        tune_args = ['']
+        settings = ['']
+
+        self.soapy_bladerf_sink_0 = soapy.sink(dev, "fc32", 1, 'bladrf=0',
+                                  stream_args, tune_args, settings)
+        self.soapy_bladerf_sink_0.set_sample_rate(0, samp_rate)
+        self.soapy_bladerf_sink_0.set_bandwidth(0, band_pass)
+        self.soapy_bladerf_sink_0.set_frequency(0, cent_freq)
+        self.soapy_bladerf_sink_0.set_frequency_correction(0, 0)
+        self.soapy_bladerf_sink_0.set_gain(0, min(max(1, 17.0), 73.0))
+        self.network_tcp_sink_1 = network.tcp_sink(gr.sizeof_gr_complex, 1, '127.0.0.1', 5001,2)
+        self.network_tcp_sink_0 = network.tcp_sink(gr.sizeof_gr_complex, 1, '127.0.0.1', 5000,2)
+        self.blocks_file_sink_0_0_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/lille-koch/Desktop/Source.bin', False)
+        self.blocks_file_sink_0_0_0.set_unbuffered(False)
+        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/lille-koch/Desktop/AT 2.bin', False)
+        self.blocks_file_sink_0_0.set_unbuffered(False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/lille-koch/Desktop/At 1.bin', False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 0.5, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.soapy_bladerf_source_0, 0), (self.zeromq_pub_sink_0, 0))
-        self.connect((self.soapy_bladerf_source_0_0, 0), (self.zeromq_pub_sink_0_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_file_sink_0_0_0, 0))
+        self.connect((self.analog_sig_source_x_0, 0), (self.soapy_bladerf_sink_0, 0))
+        self.connect((self.soapy_bladerf_source_0, 0), (self.blocks_file_sink_0_0, 0))
+        self.connect((self.soapy_bladerf_source_0, 0), (self.network_tcp_sink_1, 0))
+        self.connect((self.soapy_bladerf_source_0_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.soapy_bladerf_source_0_0, 0), (self.network_tcp_sink_0, 0))
 
 
     def get_samp_rate(self):
@@ -84,6 +110,8 @@ class Frontend(gr.top_block):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.soapy_bladerf_sink_0.set_sample_rate(0, self.samp_rate)
         self.soapy_bladerf_source_0.set_sample_rate(0, self.samp_rate)
         self.soapy_bladerf_source_0_0.set_sample_rate(0, self.samp_rate)
 
@@ -92,6 +120,7 @@ class Frontend(gr.top_block):
 
     def set_cent_freq(self, cent_freq):
         self.cent_freq = cent_freq
+        self.soapy_bladerf_sink_0.set_frequency(0, self.cent_freq)
         self.soapy_bladerf_source_0.set_frequency(0, self.cent_freq)
         self.soapy_bladerf_source_0_0.set_frequency(0, self.cent_freq)
 
@@ -100,6 +129,7 @@ class Frontend(gr.top_block):
 
     def set_band_pass(self, band_pass):
         self.band_pass = band_pass
+        self.soapy_bladerf_sink_0.set_bandwidth(0, self.band_pass)
         self.soapy_bladerf_source_0.set_bandwidth(0, self.band_pass)
         self.soapy_bladerf_source_0_0.set_bandwidth(0, self.band_pass)
 
