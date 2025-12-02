272c5c0b6965a182dd4c4ef5b2892cf05b96384d
MiniBeier <emilbeier97@gmail.com>
2025-12-02 07:03:50 +0100

    H-bridge shortcircuit plots - axis titles fixed

--- 04 Prototype udvikling/2 akset rotation/05 H-bridge/Plotting_delay.m
+++ 04 Prototype udvikling/2 akset rotation/05 H-bridge/Plotting_delay.m
@@ -5,28 +5,28 @@ close all
 load("data.mat")
 
 
+% 
+% figure
+% plot(Q4_U10_10k_res.Time/10^(-6),Q4_U10_10k_res.V1, DisplayName="PMOS")
+% hold on
+% plot(Q4_U10_10k_res.Time/10^(-6),Q4_U10_10k_res.V2, DisplayName="NMOS")
+% % hold on
+% % plot(Q4_U10_1k_res.Time/10^(-6),Q4_U10_1k_res.V1, DisplayName="PMOS + 1K")
+% % hold on
+% % plot(Q4_U10_1k_res.Time/10^(-6),Q4_U10_1k_res.V2, DisplayName="NMOS + 1K")
+% grid on
+% xlabel("Time [µs]")
+% ylabel("Voltage [V]")
+% title("H-Bridge Short Circuit Issue")
+% xlim([-10 10])
+% legend show
+% hold off
+
 
 figure
-plot(Q4_U10_10k_res.Time/10^(-6),Q4_U10_10k_res.V1, DisplayName="PMOS")
+plot(BJT_Issue_BC546.Time/10^(-6),BJT_Issue_BC546.V1, DisplayName="BJT Collector")
 hold on
-plot(Q4_U10_10k_res.Time/10^(-6),Q4_U10_10k_res.V2, DisplayName="NMOS")
-% hold on
-% plot(Q4_U10_1k_res.Time/10^(-6),Q4_U10_1k_res.V1, DisplayName="PMOS + 1K")
-% hold on
-% plot(Q4_U10_1k_res.Time/10^(-6),Q4_U10_1k_res.V2, DisplayName="NMOS + 1K")
-grid on
-xlabel("Time [µs]")
-ylabel("Voltage [V]")
-title("H-Bridge Short Circuit Issue")
-xlim([-10 10])
-legend show
-hold off
-
-
-figure
-plot(BJT_Issue_BC546.Time/10^(-6),BJT_Issue_BC546.V1, DisplayName="Issue Base")
-hold on
-plot(BJT_Issue_BC546.Time/10^(-6),BJT_Issue_BC546.V2, DisplayName="Issue Collector")
+plot(BJT_Issue_BC546.Time/10^(-6),BJT_Issue_BC546.V2, DisplayName="BJT Base")
 % hold on
 % plot(BJT_10K_res.Time/10^(-6),BJT_10K_res.V1, DisplayName="10k Base")
 % hold on
