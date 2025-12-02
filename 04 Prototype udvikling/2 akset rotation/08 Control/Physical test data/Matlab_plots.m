d5bf2eba2331319b9d7dc0aff5c1a6ed0b204973
MiniBeier <emilbeier97@gmail.com>
2025-12-02 07:07:14 +0100

    TARP Physical Control test + plots
    New data for azimuth + plot
    New data for tilt + plot

--- 04 Prototype udvikling/2 akset rotation/08 Control/Physical test data/Matlab_plots.m
+++ 04 Prototype udvikling/2 akset rotation/08 Control/Physical test data/Matlab_plots.m
@@ -19,89 +19,74 @@ CL_tilt=(d_tilt*sys_tilt)/(1+d_tilt*sys_tilt);
 
 
 %% ------- plot for TARP motor code V3.3 -----------------
-x_azi = (0:numel(x24_83TiltGain)-1)*10^(-4);
-x_tilt = (0:numel(x4_32AziGain)-1)*10^(-4);
-figure
-plot(x_azi,x4_32AziGain.Position, DisplayName="Azimut")
-hold on
-plot(x_tilt,x24_83TiltGain.Position, DisplayName="Tilt")
-hold off
-grid on
-yline(180,color='black', DisplayName="Target Angle Azimut")
-yline(160, color = 'green', DisplayName="Target Angle Tilt")
-ylabel("Position [degrees]")
-xlabel("Time [s]")
-title("P regulator Tilt vs Azimut")
-% xlim([0 4.5])
-legend show
+% x_azi = (0:numel(x24_83TiltGain)-1)*10^(-4);
+% x_tilt = (0:numel(x4_32AziGain)-1)*10^(-4);
+% figure
+% plot(x_azi,x4_32AziGain.Position, DisplayName="Azimut")
+% hold on
+% plot(x_tilt,x24_83TiltGain.Position, DisplayName="Tilt")
+% hold off
+% grid on
+% yline(180,color='black', DisplayName="Target Angle Azimut")
+% yline(160, color = 'green', DisplayName="Target Angle Tilt")
+% ylabel("Position [degrees]")
+% xlabel("Time [s]")
+% title("P regulator Tilt vs Azimut")
+% % xlim([0 4.5])
+% legend show
 
 
 
 
 %% ------- Azi plot - simulated vs reality -------------------
 % --- Real data ---
-x_azi = (0:numel(x24_83TiltGain)-1)' * 1e-4;
-real_data = x4_32AziGain.Position(:);
+x_azi = (0:numel(x4_32AziGain_20kHzPWM)-1)' * 0.0005;
 
 Ts = 1e-4;                 % same as real data
 t_azi = 0:Ts:x_azi(end);   % simulation time vector
-u = 180 * ones(size(t_azi));   % step input
+u = 90 * ones(size(t_azi));   % step input
 
 azi_sim = lsim(CL_azi, u, t_azi);
 
 % --- Plot ---
 figure
-plot(x_azi, real_data, 'DisplayName','Reality')
-hold on
-plot(t_azi, azi_sim, 'DisplayName','Simulated')
-yline(180, 'DisplayName','Target')
+plot(x_azi, x4_32AziGain_20kHzPWM.Position, 'DisplayName','Physical version')
+ hold on
+plot(t_azi, azi_sim, 'DisplayName','Simulated version')
+yline(90, 'DisplayName','Target')
 grid on
 xlabel('Time [s]')
 ylabel('Position [degrees]')
 xlim([0, 6])
-ylim([0, 300])
+ylim([0, 160])
 legend show
-title('Azimuth P regulator: simulated vs reality')
+title('Azimuth P regulator: simulated vs physical')
 
 
 %% ------- Tilt plot - simulated vs reality ---------------
-% 
-% 
-% x_tilt = (0:numel(x4_32AziGain)-1)*10^(-4);
-% 
-% r = RespConfig('Amplitude', 160);
-% [y, t] = step(CL_tilt, r);
-% y = squeeze(y);
-% 
-% if isvector(y)
-%     y = y(:);
-% end
-% 
-% % Select which output channel to plot (change idxOutput if needed)
-% idxOutput = 1;
-% tilt_sim = y(:, idxOutput);
 
 
 % --- Real data ---
-x_tilt = (0:numel(x24_83TiltGain)-1)' * 1e-4;
-real_data = x24_83TiltGain.Position(:);
+x_tilt = (0:numel(x24_83TiltGain_20kHz)-1)' *0.0005 ;
+disp('Data point:');
+disp(numel(x24_83TiltGain_20kHz));
 
 Ts = 1e-4;                 % same as real data
 t_tilt = 0:Ts:x_tilt(end);   % simulation time vector
-u = 160 * ones(size(t_tilt));   % step input
+u = 45 * ones(size(t_tilt));   % step input
 
 tilt_sim = lsim(CL_tilt, u, t_tilt);
 
 figure
-plot(x_tilt,x24_83TiltGain.Position, DisplayName="Reality")
+plot(x_tilt,x24_83TiltGain_20kHz.GiveMeAnAngle, DisplayName="Physical version")
 hold on
-plot(t_tilt, tilt_sim, 'DisplayName','Simulated')
+plot(t_tilt, tilt_sim, 'DisplayName','Simulated version')
 hold off
 grid on
-yline(160, DisplayName="Target Angle")
+yline(45, DisplayName="Target Angle")
 ylabel("Position [degrees]")
 xlabel("Time [s]")
-title("Tilt P regulator:  simulated vs reality")
+title("Tilt P regulator:  simulated vs physical")
  xlim([0 10])
 legend show
 
