b0e0526d5b0ffcd7f21a6601e4a31875f76d133a
Noah Haakon Degn Nielsen <145559591+Scramtv@users.noreply.github.com>
2025-12-10 12:28:32 +0100

    Kode kommentarer og upload af signal plot

--- 04 Prototype udvikling/DSP og beamforming/beam_forming/delay and sum beamforming/beamforming_algorithm_and_signal_simulation.py
+++ 04 Prototype udvikling/DSP og beamforming/beam_forming/delay and sum beamforming/beamforming_algorithm_and_signal_simulation.py
@@ -1,34 +1,36 @@
 import numpy as np
 
 def beamforming_das(rx, distance, no_ele):
-    theta_sweep = np.linspace(-1*np.pi/2, np.pi/2, 1000) # Deler -pi/2 til pi/2 op i 1000 segmenter og gemmer i et array
-    results = [] # Laver tomt array til resultater
-    for thetas in theta_sweep: # For alle de værdier i theta sweep, kører vi
-       w = np.exp(2j * np.pi * distance * np.arange(no_ele) * np.sin(thetas)) # Weight vektor tilsvarende "steering vektoren" køres for theta
-       y = w.conj().T @ rx # Vi kompleks konjugere for at modarbejde faseforskydningen og transponerer så matricen har den rigtige form
-       results.append(np.abs(np.sum(y))) # Vi finder modulus af de to beamformede datapunkter summeret og gemmer dem i arrayet
-    return np.rad2deg(theta_sweep[np.argmax(results)]) # Vi kigger efter den theta hvor amplituden er størst og returnerer den i grader
+    theta_sweep = np.linspace(-1*np.pi/2, np.pi/2, 1000)
+    results = []
+    for thetas in theta_sweep:
+       w = np.exp(2j * np.pi * distance * np.arange(no_ele) * np.sin(thetas))
+       y = w.conj().T @ rx
+       results.append(np.abs(np.sum(y)))
+    return np.rad2deg(theta_sweep[np.argmax(results)])
 
 sample_rate = 61.44e6
-N = 10000
-t = np.arange(N)/sample_rate
+N = 10000 # samples to take
+t = np.arange(N)/sample_rate # time
 
 ### TX SIGNAL
-theta_deg = 0 # Angle of arrival
+theta_deg = 0 # angle of arrival
 theta_rad = np.deg2rad(theta_deg)
-f = 2e4
-tx = np.exp(2j * np.pi * f * t)
-d = 0.5
+f = 2e4 # baseband signal frequency
+tx = np.exp(2j * np.pi * f * t) # x(t) baseband signal
+
+### ARRAY
 elements = 2
-k = np.arange(elements)
-s = np.exp(2j * np.pi * d * k * np.sin(theta_rad))
-s = s.reshape(-1,1)
-tx = tx.reshape(1,-1)
+d = 0.5 # distance in wavelengths between elements
+k = np.arange(elements) # element index
+s = np.exp(2j * np.pi * d * k * np.sin(theta_rad)) # steering vector
+s = s.reshape(-1,1) # reshape s into a column vector
+tx = tx.reshape(1,-1) # reshape tx into a row vector
 
 ### RX SIGNAL
-X = s @ tx
-n = np.random.randn(elements, N) + 1j*np.random.randn(elements, N)
-X_n = X + 0.0 * n
+X = s @ tx # matrix multiply the steering vector on the transmit signal to make the received signal
+n = np.random.randn(elements, N) + 1j*np.random.randn(elements, N) #generate real and imaginary awgn
+X_n = X + 0.1 * n # apply noise to the received signal
 
 ### FUNCTIONALITY TEST
 bf_signal = beamforming_das(X_n, d, elements)
