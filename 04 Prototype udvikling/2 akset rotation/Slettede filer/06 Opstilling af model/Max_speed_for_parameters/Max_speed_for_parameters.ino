67e54b52672c0998a088e0b97ef86335771c74f5
HelgeCl <249914276+HelgeCl@users.noreply.github.com>
2025-12-15 23:48:20 +0100

    Nogen har fået fucket up i git, så disse filer forsvandt
    Jeg har nu genoprettet dem i en "slette mappe" for ikke at overskrive andre ændringer

---
+++ 04 Prototype udvikling/2 akset rotation/Slettede filer/06 Opstilling af model/Max_speed_for_parameters/Max_speed_for_parameters.ino
@@ -0,0 +1,158 @@
+//encoder_Azimut pins
+static int pinA_azi = 38;  //Pin A
+static int pinB_azi = 40;  //Pin B
+static int pinZ_azi = 37;  //Pin Z
+
+//Motor_Azimut pins:
+static int ena_pin_azi = 13;  //enable pin controls the motor speed with PWM
+static int in1_azi = 14;      // logic input 1
+
+//encoder_tilt pins
+static int pinA_tilt = 26;  //Pin A
+static int pinB_tilt = 33;  //Pin B
+static int pinZ_tilt = 21;  //Pin Z
+
+//Encoder_tilt_motor pins
+static int pinA_tilt_motor = 20;  //Pin A
+
+//Motor_tilt pins:
+static int ena_pin_tilt = 16;  //enable pin controls the motor speed with PWM
+static int in1_tilt = 18;      // logic input 1
+static int in2_tilt = 17;      //logic input 2
+
+//btn
+static int btn_cw_limit = 41;
+static int btn_ccw_limit = 42;
+
+//-------------------------------------------------------------------------------//
+#include "soc/gpio_struct.h"
+#include "driver/gpio.h"
+#include <WiFi.h>
+// Fast read
+#define READ_PIN(pin) ((pin) < 32 ? ((GPIO.in >> (pin)) & 1) : ((GPIO.in1.val >> ((pin)-32)) & 1))
+
+//Vars for encoder azimut
+volatile int pos_azi = 0;
+volatile int rot_azi = 0;
+
+//Vars for encoder tilt
+volatile int pos_tilt = 0;
+volatile int rot_tilt = 0;
+
+//Vars for encoder tilt motor
+volatile int pos_tilt_motor = 0;
+
+WiFiServer server(1234);  // TCP server on port 1234
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);  //wait for serialport
+  Serial.println("Booted");
+
+  pinSetup();
+  attachInt();
+
+  init_wireless();
+
+  pinMode(46, OUTPUT);
+  digitalWrite(46,0);
+}
+
+unsigned long lastrun = 0;
+unsigned long interval = 10;
+
+
+
+void loop() {
+  WiFiClient client = server.available();
+
+  // Wait for a client to connect
+  if (client) {
+    // If client is connected, send data
+    client.println("Hello PC! ESP32 AP connected.");
+    Serial.println("Connected to PC");
+    minVoltageTest(client);
+  }
+}
+
+void tiltVelocityTest(WiFiClient client) {
+  // VELOCITY TEST TILT MOTOR
+  digitalWrite(ena_pin_tilt, 1);
+  
+  while(!client.available()){};
+  String inputString = client.readStringUntil('\n');
+  inputString.trim();
+  int pwm_value = inputString.toInt();
+
+  analogWrite(in1_tilt, pwm_value);
+
+  digitalWrite(46, 1);
+  lastrun = millis();
+  while (true) {
+    if (millis() - lastrun > interval) {
+      lastrun += interval;
+      client.print(millis());
+      client.print(";");
+      client.print(pos_tilt);
+      client.print(";");
+      client.print(rot_tilt);
+      client.println(";");
+    }
+
+  }
+}
+
+void azimutVelocityTest(WiFiClient client) {
+  digitalWrite(ena_pin_azi, 1);
+
+  while(!client.available()){};
+  String inputString = client.readStringUntil('\n');
+  inputString.trim();
+  int pwm_value = inputString.toInt();
+
+  analogWrite(in1_azi, pwm_value);
+  digitalWrite(46, 1);
+  lastrun = millis();
+  while (true) {
+    if (millis() - lastrun > interval) {
+      lastrun += interval;
+      client.print(millis());
+      client.print(";");
+      client.print(pos_azi);
+      client.print(";");
+      client.print(rot_azi);
+      client.println(";");
+    }
+  }
+}
+
+void minVoltageTest(WiFiClient client) {
+  while (true) {
+    // 1. Read all characters until a newline is found
+    String data = client.readStringUntil('\n');
+    data.trim();  // Remove any extra spaces, \r, or \n characters
+
+    // 2. Convert the string of characters into an integer number
+    int pwmValue = data.toInt();
+    bool direction;
+    if(pwmValue < 0){
+      pwmValue = pwmValue*(-1);
+      direction = 0; //switch direction
+    }
+    else{
+      direction = 1;
+    }
+
+    if (pwmValue > 0 && pwmValue < 511) {
+      client.print("Received: ");
+      client.println(pwmValue);
+      analogWrite(ena_pin_azi, pwmValue);
+      digitalWrite(in1_azi, direction);
+//    digitalWrite(in2_tilt, !direction);
+
+      float temp = (float)pwmValue * 12 / 511;
+      client.print("Voltage set to: ");
+      client.println(temp); 
+    }
+  }
+}
